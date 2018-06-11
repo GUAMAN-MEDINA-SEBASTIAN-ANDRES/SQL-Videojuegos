86a4ce901a44db53fe8526606eb64f922988d23f
SEBASTIANGUAMAN <36029535+SEBASTIANGUAMAN@users.noreply.github.com>
2018-06-11 00:25:41 -0500

    Update Postgres.sql

--- Postgres.sql
+++ Postgres.sql
@@ -1,21 +1,53 @@
-
 CREATE DATABASE VideoJUegos;
 USE VideoJUegos;
-CREATE TABLE Proceso (
- id INT PRIMARY KEY , 
- tipo VARCHAR(20) not null, 
- tiempoDeDiseño varchar(20)not null,
- tiempoDePlanificacion Varchar(20)not null,
- nombreDelJuego varchar (20)not null
+
+CREATE TABLE Plataformas(
+ id   int not null primary KEY auto_increment , 
+ tipoPlataforma VARCHAR(50) not null, 
+ nombrePlataforma varchar(50)not null,
+ idConstructores1 int
+);
+
+INSERT INTO Plataformas (id,tipoPlataforma,nombrePlataforma,idConstructores1 ) 
+VALUES ( 1, 'PC','PC',1);
+select * from Plataformas ;
+
+CREATE TABLE Constructores(
+ id   int not null primary KEY auto_increment , 
+ nombreEmpresa VARCHAR(20) not null,
+ idPlataformas int not null
  );
- INSERT INTO Proceso (id,tipo,tiempoDeDiseño,tiempoDePlanificacion,nombreDelJuego) 
- VALUES ( 1, 'terror','40h','70h','Crocuro' );
- INSERT INTO Proceso (id,tipo,tiempoDeDiseño,tiempoDePlanificacion,nombreDelJuego) 
- VALUES ( 2, 'rol','40h','70h','warrior' );
- INSERT INTO Proceso (id,tipo,tiempoDeDiseño,tiempoDePlanificacion,nombreDelJuego) 
- VALUES ( 3, 'tersera persona','40h','70h','3D My eyes' );
- INSERT INTO Proceso (id,tipo,tiempoDeDiseño,tiempoDePlanificacion,nombreDelJuego) 
- VALUES ( 4, 'misterio','40h','70h','Camino hacia la nostalgia' );
  
- select id, tipo from Proceso where id=1; 
- alter table Revicion add N°Mantenimientos int default true;
+INSERT INTO Constructores (id,nombreEmpresa,idPlataformas ) 
+VALUES ( 1, 'VALVE',1);
+
+CREATE TABLE Torneos(
+ id   int not null primary KEY auto_increment , 
+ ubicacion VARCHAR(20) not null, 
+ nombrejuego varchar(20)not null,
+ premio varchar(20) not null,
+ idConstructores1 int
+);
+
+INSERT INTO Torneos (id,ubicacion,nombrejuego,premio,idConstructores1) 
+VALUES ( 1, 'consola','Dota2', '1 millon de dolares',1);
+
+CREATE TABLE Comunidades(
+ id   int not null primary KEY auto_increment , 
+ nombreForos VARCHAR(20) not null, 
+ idTorneos1 int not null
+);
+
+INSERT INTO Comunidades (id,nombreForos,idTorneos1) 
+VALUES ( 1, 'dOTAPLUS',1);
+ 
+ CREATE TABLE PlataformasPorConstructores(
+ id   int not null  ,
+ idConstructores int not null,
+ idPlataformas int not null,
+ FOREIGN KEY ( idConstructores) REFERENCES Constructores( id),
+ FOREIGN KEY ( idPlataformas) REFERENCES Plataformas( id)
+);
+
+INSERT INTO PlataformasPorConstructores (id,idConstructores,idPlataformas ) 
+VALUES ( 1, 1, 1);
